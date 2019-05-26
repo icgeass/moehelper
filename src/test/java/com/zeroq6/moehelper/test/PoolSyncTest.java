dea9c04c6d852679db0973549bdc75c131086f21
icgeass <icgeass@hotmail.com>
2019-05-26 13:11:43 +0800

    pool同步测试微调

--- src/test/java/com/zeroq6/moehelper/test/PoolSyncTest.java
+++ src/test/java/com/zeroq6/moehelper/test/PoolSyncTest.java
@@ -17,11 +17,12 @@ public class PoolSyncTest {
 
     /**
      * 同步pool
-     * oldDir中被替换回来的，会放置在oldDir同级目录下的，yyyyMMddHHmmss.old.replaced.to_delete_manual，确认无误，手动删除即可
+     * oldDir中被替换回来的，会放置在oldDir同级目录下的，oldDir_yyyyMMddHHmmss_to_delete，确认无误，手动删除即可
+     *
      * @throws Exception
      */
     @Test
-    public void sync() throws Exception{
+    public void sync() throws Exception {
         // 最近更新的pool
         File newDir = new File("H:\\yande.re\\yande.re_-_Pool_20190521");
         // 以前更新的pool，如果是最终移动到Pool_Packages，需要设定为Pool_Packages目录
@@ -36,24 +37,24 @@ public class PoolSyncTest {
         }
         // 校验文件名
         String reg = "yande[.]re_[-]_Pool_\\d{8}";
-        if(!Pattern.matches(reg, newDir.getName())){
+        if (!Pattern.matches(reg, newDir.getName())) {
             throw new RuntimeException("新目录名必须满足正则" + reg);
         }
-        if(finalToPoolPackages){
+        if (finalToPoolPackages) {
             String poolPackages = "Pool_Packages";
-            if(!oldDir.getName().equals(poolPackages)){
+            if (!oldDir.getName().equals(poolPackages)) {
                 throw new RuntimeException("旧目录名必须为" + poolPackages);
             }
-        }else{
-            if(!Pattern.matches(reg, oldDir.getName())){
+        } else {
+            if (!Pattern.matches(reg, oldDir.getName())) {
                 throw new RuntimeException("旧目录名必须满足正则" + reg);
             }
-            if(newDir.getName().compareTo(oldDir.getName()) <= 0){
+            if (newDir.getName().compareTo(oldDir.getName()) <= 0) {
                 throw new RuntimeException("新目录比较必须大于旧目录");
             }
         }
         // 校验空间
-        if(FileUtils.sizeOfDirectory(newDir) >= oldDir.getFreeSpace() - (FileUtils.ONE_GB * 10)){
+        if (FileUtils.sizeOfDirectory(newDir) >= oldDir.getFreeSpace() - (FileUtils.ONE_GB * 10)) {
             throw new RuntimeException("目标空间不足");
         }
         Map<String, List<File>> newMap = transferIdFileMap(FileUtils.listFiles(newDir, suffix, true));
@@ -63,17 +64,17 @@ public class PoolSyncTest {
         for (Map.Entry<String, List<File>> item : newMap.entrySet()) {
             List<File> old = oldMap.get(item.getKey());
             if (null == old) {
-                for(File f : item.getValue()){
+                for (File f : item.getValue()) {
                     FileUtils.moveFileToDirectory(f, finalToPoolPackages ? genMoveToDirById(Integer.valueOf(item.getKey()), oldDir) : oldDir, true);
                     System.out.println("new新增: " + f.getAbsolutePath());
                 }
             } else {
                 // 先移除, 后新增, 避免文件名同步无法新增或错误覆盖
-                for(File f : old){
+                for (File f : old) {
                     System.out.println("update移除: " + f.getAbsolutePath());
-                    FileUtils.moveFileToDirectory(f, new File(oldDir.getParentFile().getCanonicalPath() + File.separator + time + ".old.replaced.to_delete_manual"), true);
+                    FileUtils.moveFileToDirectory(f, new File(oldDir.getParentFile().getCanonicalPath() + File.separator + oldDir.getName() + "_" + time + "_to_delete"), true);
                 }
-                for(File f : item.getValue()){
+                for (File f : item.getValue()) {
                     System.out.println("update新增: " + f.getAbsolutePath());
                     FileUtils.moveFileToDirectory(f, finalToPoolPackages ? genMoveToDirById(Integer.valueOf(item.getKey()), oldDir) : oldDir, true);
                 }
@@ -83,14 +84,14 @@ public class PoolSyncTest {
 
     }
 
-    private File genMoveToDirById(int id, File toDir) throws Exception{
+    private File genMoveToDirById(int id, File toDir) throws Exception {
         if (id < 1) {
             throw new RuntimeException("id非法, " + id);
         }
         int index = id % 600 == 0 ? id / 600 : id / 600 + 1;
         int a = (index - 1) * 600 + 1;
         int b = index * 600;
-        String name = String.format("Pool_Packages-%s(id=%s-%s)", StringUtils.leftPad(index+"",2,"0"), a + "", b + "");
+        String name = String.format("Pool_Packages-%s(id=%s-%s)", StringUtils.leftPad(index + "", 2, "0"), a + "", b + "");
         return new File(toDir.getCanonicalPath() + File.separator + name);
 
     }
@@ -106,7 +107,7 @@ public class PoolSyncTest {
                 throw new RuntimeException("非法文件名, " + name);
             }
             String id = Integer.valueOf(name.substring(1, 5)) + "";
-            if(null == result.get(id)){
+            if (null == result.get(id)) {
                 result.put(id, new ArrayList<File>());
             }
             result.get(id).add(file);
